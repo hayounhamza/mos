9dd9f136f2c71e93a194f548d093050f8bc8eb29
hayounhamza <hamzathp6@gmail.com>
2025-06-02 16:40:43 +0100

    edits

--- android/app/src/main/java/com/appmosafir/MainActivity.java
+++ android/app/src/main/java/com/appmosafir/MainActivity.java
@@ -2,32 +2,17 @@ package com.appmosafir;
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.defaults.DefaultReactActivityDelegate;
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
-
-// 👇 use gesture-handler wrapper, not expo
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
   @Override
   protected String getMainComponentName() {
-    return "Mosafir.ma"; // should match "name" in app.json
+    return "Mosafir.ma"; // must match the "name" in app.json
   }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    // Required for react-native-gesture-handler and react-native-screens
     super.onCreate(null);
-  }
-
-  @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegate(this, getMainComponentName()) {
-      @Override
-      protected RNGestureHandlerEnabledRootView createRootView() {
-        return new RNGestureHandlerEnabledRootView(MainActivity.this);
-      }
-    };
   }
 }
